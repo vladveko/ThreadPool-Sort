c913d7446253f7c5e877ef54d26097d788f3062e
vlad_veko <veko.2001@gmail.com>
2020-11-16 22:48:46 +0300

    Initial commit

---
+++ ThreadPool.cpp
@@ -0,0 +1,58 @@
+﻿#include "ThreadPool.h"
+#include <iostream>
+
+ThreadPool::ThreadPool() {
+	refCount = 0;
+	isRunning = true;
+	mtx = CreateMutex(NULL, false, NULL);
+
+	for (int i = 0; i < DEF_THREAD_NUM;i++) {
+		HANDLE hThread = CreateThread(
+			NULL, 0, ThreadStartRoutine, (void*)this, 0, 0);
+		threads[i] = hThread;
+	}
+}
+
+ThreadPool::~ThreadPool() {
+	
+}
+
+static DWORD WINAPI ThreadStartRoutine(LPVOID lpParam) {
+	ThreadPool* pObj = (ThreadPool*)lpParam;
+	return pObj->AwaitLoop;
+}
+
+void ThreadPool::AwaitLoop() {
+	void (*task)(LPVOID taskParam) = NULL;
+	LPVOID taskParam = NULL;
+
+	while (isRunning) {
+		WaitForSingleObject(mtx, INFINITE);
+		{
+			if (taskQueue.size() != 0)
+			{
+				task = this->taskQueue.front();
+				taskParam = this->taskParams.front();
+
+				this->taskQueue.pop_front();
+				this->taskParams.pop_front();
+			}
+		}
+		ReleaseMutex(mtx);
+
+		if (task != NULL) {
+			InterlockedIncrement(&refCount);
+			task(taskParam);
+			InterlockedDecrement(&refCount);
+		}
+	}
+	ExitThread(0);
+}
+
+void ThreadPool::EnqueueTask(void (*task)(LPVOID lpParam), LPVOID lpParam) { 
+	WaitForSingleObject(mtx, INFINITE);
+	{
+
+	}
+	ReleaseMutex(mtx);
+}
---
+++ ThreadPool.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Windows.h>
+#include <list>
+
+#define DEF_THREAD_NUM 5
+
+class ThreadPool {
+
+	static DWORD WINAPI ThreadStartRoutine(LPVOID lpParam);
+
+private:
+	bool isRunning;
+	unsigned int refCount;
+	std::list<void (*)(LPVOID lpParam)> taskQueue;
+	std::list<LPVOID> taskParams;
+	HANDLE threads[DEF_THREAD_NUM];
+	HANDLE mtx;
+
+	void AwaitLoop();
+
+public:
+	ThreadPool();
+	~ThreadPool();
+
+	void EnqueueTask(void (*task)(LPVOID lpParam), LPVOID lpParam);
+	void WaitAll();
+};
